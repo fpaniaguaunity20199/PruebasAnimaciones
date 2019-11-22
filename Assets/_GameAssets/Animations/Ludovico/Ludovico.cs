0bfdbf7695ef93f763fac03f7539436d70403150
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-11-22 11:32:23 +0100

    2019-11-21-FIN

---
+++ Assets/_GameAssets/Animations/Ludovico/Ludovico.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ludovico : MonoBehaviour
+{
+    public float pesoSpeed;
+    private Animator animatorLudovico;
+    private float peso=0.0f;
+    void Start()
+    {
+        animatorLudovico = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            peso = peso + Time.deltaTime * pesoSpeed;
+            peso = Mathf.Min(1, peso);
+        } else
+        {
+            peso = peso - Time.deltaTime * pesoSpeed;
+            peso = Mathf.Max(0, peso);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animatorLudovico.SetBool("Running", true);
+        } else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animatorLudovico.SetBool("Running", false);
+        }
+        animatorLudovico.SetLayerWeight(1, peso);
+    }
+}
---
+++ Assets/_GameAssets/Scripts/Adam.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adam : MonoBehaviour
+{
+    bool enPosSaltoObstaculoBajo = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (enPosSaltoObstaculoBajo)
+            {
+                GetComponent<Animator>().SetTrigger("Saltar");
+            }
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("ObstaculoBajo"))
+        {
+            enPosSaltoObstaculoBajo = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("ObstaculoBajo"))
+        {
+            enPosSaltoObstaculoBajo = false;
+        }
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("ObstaculoBajo"))
+    //    {
+    //        print("SALTANDO UN OBSTACULO BAJO");
+    //        GetComponent<Animator>().SetTrigger("Saltar");
+    //    }
+    //    if (other.CompareTag("ObstaculoAlto"))
+    //    {
+    //        print("SALTANDO UN OBSTACULO ALTO");
+    //        GetComponent<Animator>().SetTrigger("Subir");
+    //    }
+    //}
+}
